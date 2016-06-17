2890e181219d06fd2894f839707658be6b620b83
develope@foxmail.com <develope@foxmail.com>
2016-06-17 16:27:03 +0800

    上传图片后的显示地址

--- Addons/EditorForAdmin/Controller/UploadController.class.php
+++ Addons/EditorForAdmin/Controller/UploadController.class.php
@@ -8,59 +8,69 @@
 // +----------------------------------------------------------------------
 
 namespace Addons\EditorForAdmin\Controller;
+
 use Home\Controller\AddonsController;
 use Think\Upload;
 
-class UploadController extends AddonsController{
+class UploadController extends AddonsController
+{
 
-	public $uploader = null;
+    public $uploader = null;
 
-	/* 上传图片 */
-	public function upload(){
-		/* 上传配置 */
-		$setting = C('EDITOR_UPLOAD');
+    /* 上传图片 */
+    public function upload()
+    {
+        /* 上传配置 */
+        $setting = C('EDITOR_UPLOAD');
 
-		/* 调用文件上传组件上传文件 */
+        /* 调用文件上传组件上传文件 */
         $this->uploader = new Upload($setting);
-		$info   = $this->uploader->upload($_FILES);
-		if($info){
-			$url = $info['imgFile']['savepath'].$info['imgFile']['savename'];
-			$url = str_replace('./', '/', $url);
-            $info['fullpath'] = 'http://' . $_SERVER['HTTP_APPNAME'] . '-uploads.stor.sinaapp.com/Editor/'.$url;
-		}
-		return $info;
-	}
+        $info = $this->uploader->upload($_FILES);
+        if ($info) {
+            $url = $info['imgFile']['savepath'] . $info['imgFile']['savename'];
+            $url = str_replace('./', '/', $url);
 
-	//keditor编辑器上传图片处理
-	public function ke_upimg(){
-		/* 返回标准数据 */
-		$return  = array('error' => 0, 'info' => '上传成功', 'data' => '');
-		$img = $this->upload();
-		/* 记录附件信息 */
-		if($img){
-			$return['url'] = $img['fullpath'];
-			unset($return['info'], $return['data']);
-		} else {
-			$return['error'] = 1;
-			$return['message']   = $this->uploader->getError();
-		}
+            if (function_exists('saeAutoLoader')) {
+                $info['fullpath'] = 'http://' . $_SERVER['HTTP_APPNAME'] . '-uploads.stor.sinaapp.com/Editor/' . $url;
+            } else {
+                $info['fullpath'] = __ROOT__ . '/Uploads/Editor/' . $url;
+            }
+        }
+        return $info;
+    }
 
-		/* 返回JSON数据 */
-		exit(json_encode($return));
-	}
+    //keditor编辑器上传图片处理
+    public function ke_upimg()
+    {
+        /* 返回标准数据 */
+        $return = array('error' => 0, 'info' => '上传成功', 'data' => '');
+        $img = $this->upload();
+        /* 记录附件信息 */
+        if ($img) {
+            $return['url'] = $img['fullpath'];
+            unset($return['info'], $return['data']);
+        } else {
+            $return['error'] = 1;
+            $return['message'] = $this->uploader->getError();
+        }
 
-	//ueditor编辑器上传图片处理
-	public function ue_upimg(){
+        /* 返回JSON数据 */
+        exit(json_encode($return));
+    }
 
-		$img = $this->upload();
-		$return = array();
-		$return['url'] = $img['fullpath'];
-		$title = htmlspecialchars($_POST['pictitle'], ENT_QUOTES);
-		$return['title'] = $title;
-		$return['original'] = $img['imgFile']['name'];
-		$return['state'] = ($img)? 'SUCCESS' : $this->uploader->getError();
-		/* 返回JSON数据 */
-		$this->ajaxReturn($return);
-	}
+    //ueditor编辑器上传图片处理
+    public function ue_upimg()
+    {
+
+        $img = $this->upload();
+        $return = array();
+        $return['url'] = $img['fullpath'];
+        $title = htmlspecialchars($_POST['pictitle'], ENT_QUOTES);
+        $return['title'] = $title;
+        $return['original'] = $img['imgFile']['name'];
+        $return['state'] = ($img) ? 'SUCCESS' : $this->uploader->getError();
+        /* 返回JSON数据 */
+        $this->ajaxReturn($return);
+    }
 
 }
