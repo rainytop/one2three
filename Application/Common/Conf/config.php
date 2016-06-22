82f888db01a841b15ded4d325c796d8561f83b25
notinmood <develope@live.com>
2016-06-22 09:01:08 +0800

    修改数据库连接

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -53,7 +53,7 @@ return array(
 
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => '127.0.0.1', // 服务器地址
-    'DB_NAME' => 'one2three', // 数据库名
+    'DB_NAME' => 'my_one2three', // 数据库名
     'DB_USER' => 'root', // 用户名
     'DB_PWD' => 'GQxB4LyasPhD8rqN', // 密码
     'DB_PORT' => '3306', // 端口
